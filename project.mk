41b1f5f70349b51ccd4c49f2671fff5fb24b5ede
sadko4u <sadko4u@gmail.com>
2020-05-03 00:07:13 +0300

    Initial code import

---
+++ project.mk
@@ -0,0 +1,15 @@
+# Package version
+ARTIFACT_NAME               = lsp-r3d-base-lib
+ARTIFACT_VARS               = LSP_R3D_BASE_LIB
+ARTIFACT_HEADERS            = lsp-plug.in
+ARTIFACT_EXPORT_ALL         = 1
+VERSION                     = 0.5.0-devel
+
+# List of dependencies
+TEST_DEPENDENCIES = \
+  TEST_STDLIB \
+  LSP_TEST_FW
+
+DEPENDENCIES = \
+  STDLIB \
+  LSP_COMMON_LIB
